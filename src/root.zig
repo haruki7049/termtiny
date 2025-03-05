9cba11336ee0bf97904e6ed4ae434e80a47b3836
haruki7049 <tontonkirikiri@gmail.com>
2025-03-05 22:43:55 +0900

    WIP

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,47 @@
 const std = @import("std");
-const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+/// A mouse related event
+pub const MouseEvent = union(enum) {
+    /// A mouse button was pressed
+    Press: struct {
+        button: MouseButton,
+        x: u16,
+        y: u16,
+    },
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+    /// A mouse button was released
+    Release: struct {
+        x: u16,
+        y: u16,
+    },
+
+    /// A mouse button is held over the given coordinates
+    Hold: struct {
+        x: u16,
+        y: u16,
+    },
+};
+
+/// A mouse button
+pub const MouseButton = enum {
+    /// The left mouse button
+    Left,
+
+    /// The right mouse button
+    Right,
+
+    /// The middle mouse button.
+    Middle,
+
+    /// Mouse wheel is going up
+    WheelUp,
+
+    /// Mouse wheel is going down
+    WheelDown,
+
+    /// Mouse wheel is going left
+    WheelLeft,
+
+    /// Mouse wheel is going right
+    WheelRight,
+};
